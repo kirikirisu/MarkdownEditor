c36122175c30e159b73634fb47abede6c9bfc384
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2021-02-01 17:40:45 +0900

    add eslint and markdown Parser

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+/* eslint-disable import/no-unresolved */
+import React, { useState } from 'react';
+import AceEditor from 'react-ace';
+import marked from 'marked';
+
 import './App.css';
 
-function App() {
+import 'ace-builds/src-noconflict/mode-java';
+import 'ace-builds/src-noconflict/theme-github';
+
+const onChangeEditorText = (md, setHtml, setMd) => {
+  setMd(md);
+  setHtml(marked(md));
+};
+
+const App = () => {
+  const [inputMd, setMd] = useState('');
+  const [html, setHtml] = useState('');
+
+  const renderPreview = () => <div dangerouslySetInnerHTML={{ __html: html }} />;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <AceEditor
+        mode="java"
+        theme="github"
+        name="mdEditor"
+        width="50%"
+        height="100%"
+        value={inputMd}
+        onChange={(md) => onChangeEditorText(md, setHtml, setMd)}
+      />
+      <div className="preview">{renderPreview()}</div>
     </div>
   );
-}
+};
 
 export default App;
