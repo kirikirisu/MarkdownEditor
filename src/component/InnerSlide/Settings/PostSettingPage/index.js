2fb209a0c8bf3276a5e2dbe5d49420cc768bce8a
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2021-02-10 19:31:42 +0900

    add axios

--- src/component/InnerSlide/Settings/PostSettingPage/index.js
+++ src/component/InnerSlide/Settings/PostSettingPage/index.js
@@ -1,10 +1,11 @@
+/* eslint-disable array-callback-return */
 /* eslint-disable react/button-has-type */
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable max-len */
 /* eslint-disable consistent-return */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable import/no-cycle */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 import InputText from './InputText';
@@ -43,6 +44,7 @@ const SwitchInputType = (props) => {
 };
 
 const PostSettingPage = ({ presets, currentPreset }) => {
+  // const [parsedPosts, setParsedPosts] = useState({});
   const [postValues, setPostValues] = useState({});
   let preset;
 
@@ -51,14 +53,26 @@ const PostSettingPage = ({ presets, currentPreset }) => {
     preset = pre;
   }
 
+  useEffect(() => {
+    setPostValues({});
+  }, [currentPreset]);
+
+  // TODO フィールドの名前をかぶらないようにする(shemasettingでバリデーション)
   const post = async (e) => {
     e.preventDefault();
+    const elements = Object.keys(postValues).map((id) => postValues[id]);
+    const posts = {};
 
-    const blogs = {
-    };
+    elements.map((el) => {
+      const ky = Object.keys(el)[0];
+      const vl = el[ky];
+
+      posts[ky] = vl;
+    });
+    console.log('result', posts);
   };
 
-  console.log(postValues);
+  console.log('postV', postValues);
 
   return (
     <div className={styles.container}>
@@ -66,38 +80,40 @@ const PostSettingPage = ({ presets, currentPreset }) => {
         (presets.length === 0) ? (
           <div>プリセットを追加してください</div>
         ) : (
-          <div className={styles.inputsContainer}>
-            <div className={styles.title}>Postする値を設定</div>
-            <div className={styles.inputList}>
-              {
-                Object.keys(preset).map((id) => {
-                  const field = preset[id];
-                  const fieldName = Object.keys(field)[0];
+          <div>
+            <div className={styles.inputsContainer}>
+              <div className={styles.title}>Postする値を設定</div>
+              <div className={styles.inputList}>
+                {
+                  Object.keys(preset).map((id) => {
+                    const field = preset[id];
+                    const fieldName = Object.keys(field)[0];
 
-                  return (
-                    <div key={id} className={styles.inputs}>
-                      <SwitchInputType
-                        id={id}
-                        field={field}
-                        fieldName={fieldName}
-                        setPostValues={setPostValues}
-                        postValues={postValues}
-                      />
-                    </div>
-                  );
-                })
-              }
+                    return (
+                      <div key={id} className={styles.inputs}>
+                        <SwitchInputType
+                          id={id}
+                          field={field}
+                          fieldName={fieldName}
+                          setPostValues={setPostValues}
+                          postValues={postValues}
+                        />
+                      </div>
+                    );
+                  })
+                }
+              </div>
             </div>
+            <div className={styles.setUrl}>
+              <div className={styles.urlTitle}>URL</div>
+              <div className={styles.url}>
+                url format
+              </div>
+            </div>
+            <button onClick={(e) => post(e)} className={styles.postButton}>POST</button>
           </div>
         )
       }
-      <div className={styles.setUrl}>
-        <div className={styles.urlTitle}>URL</div>
-        <div className={styles.url}>
-          url format
-        </div>
-      </div>
-      <button onClick={(e) => post(e)} className={styles.postButton}>POST</button>
     </div>
   );
 };
